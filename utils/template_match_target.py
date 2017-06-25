06739a793c421e6e23a70c09bf74279977e99c8f
silburt <silburt@astro.utoronto.ca>
2017-06-25 12:24:24 -0400

    edits

--- utils/template_match_target.py
+++ utils/template_match_target.py
@@ -6,24 +6,23 @@ import numpy as np
 from skimage.feature import match_template
 import cv2
 
-def template_match_target_to_csv(target, csv_coords, minrad=2, maxrad=75):
+def template_match_target(target, match_thresh2=20, minrad=2, maxrad=75):
+    #Match Threshold (squared)
+    # for template matching, if (x1-x2)^2 + (y1-y2)^2 + (r1-r2)^2 < match_thresh2, remove (x2,y2,r2) circle (it is a duplicate).
+    # for predicted target -> csv matching, if (x1-x2)^2 + (y1-y2)^2 + (r1-r2)^2 < match_thresh2, positive detection
+    
     # minrad/maxrad are the radii to search over during template matching
     # hyperparameters, probably don't need to change
     ring_thickness = 2       #thickness of rings for the templates. 2 seems to work well.
     template_thresh = 0.5    #0-1 range, if template matching probability > template_thresh, count as detection
     target_thresh = 0.1      #0-1 range, pixel values > target_thresh -> 1, pixel values < target_thresh -> 0
     
-    #Match Threshold (squared)
-    # for template matching, if (x1-x2)^2 + (y1-y2)^2 + (r1-r2)^2 < match_thresh2, remove (x2,y2,r2) circle (it is a duplicate).
-    # for predicted target -> csv matching, if (x1-x2)^2 + (y1-y2)^2 + (r1-r2)^2 < match_thresh2, positive detection
-    match_thresh2 = 20
-    
     # target - can be predicted or ground truth
     target[target >= target_thresh] = 1
     target[target < target_thresh] = 0
     
     radii = np.linspace(minrad,maxrad,maxrad-minrad,dtype=int)
-    templ_coords = []        #coordinates extracted from template matching
+    coords = []        #coordinates extracted from template matching
     for r in radii:
         # template
         n = 2*(r+ring_thickness+1)
@@ -36,17 +35,29 @@ def template_match_target_to_csv(target, csv_coords, minrad=2, maxrad=75):
         
         # store x,y,r
         for c in coords_r:
-            templ_coords.append([c[1],c[0],r])
+            coords.append([c[1],c[0],r])
 
     # remove duplicates from template matching at neighboring radii/locations
-    templ_coords = np.asarray(templ_coords)
-    i, N = 0, len(templ_coords)
+    coords = np.asarray(coords)
+    i, N = 0, len(coords)
     while i < N:
-        diff = (templ_coords - templ_coords[i])**2
+        diff = (coords - coords[i])**2
         diffsum = np.asarray([sum(x) for x in diff])
         index = (diffsum == 0)|(diffsum > match_thresh2)
-        templ_coords = templ_coords[index]
-        N, i = len(templ_coords), i+1
+        coords = coords[index]
+        N, i = len(coords), i+1
+
+    return coords
+
+
+def template_match_target_to_csv(target, csv_coords, minrad=2, maxrad=75):
+    #Match Threshold (squared)
+    # for template matching, if (x1-x2)^2 + (y1-y2)^2 + (r1-r2)^2 < match_thresh2, remove (x2,y2,r2) circle (it is a duplicate).
+    # for predicted target -> csv matching, if (x1-x2)^2 + (y1-y2)^2 + (r1-r2)^2 < match_thresh2, positive detection
+    match_thresh2 = 20
+    
+    #get coordinates from template matching
+    templ_coords = template_match_target(target, match_thresh2, minrad=2, maxrad=75)
 
     # compare template-matched results to "ground truth" csv input data
     N_match = 0
@@ -64,7 +75,8 @@ def template_match_target_to_csv(target, csv_coords, minrad=2, maxrad=75):
         N_match += N
         csv_coords = csv_coords[index]
         if len(csv_coords) == 0:
-            #print "all matches found"
             break
 
     return N_match, N_csv, N_templ, csv_duplicate_flag
+
+
