43e32e79e9e94f515f6a58d5873513db466cbe16
silburt <silburt@astro.utoronto.ca>
2017-06-25 22:48:01 -0400

    test

--- run_moon_convnet_model.py
+++ run_moon_convnet_model.py
@@ -207,17 +207,17 @@ if __name__ == '__main__':
     dir = 'dataset'         #location of Train_rings/, Dev_rings/, Test_rings/, Dev_rings_for_loss/ folders. Don't include final '/' in path
     lr = 0.0001             #learning rate
     bs = 32                 #batch size: smaller values = less memory but less accurate gradient estimate
-    epochs = 6              #number of epochs. 1 epoch = forward/back pass through all train data
-    n_train = 6016          #number of training samples, needs to be a multiple of batch size. Big memory hog.
+    epochs = 2              #number of epochs. 1 epoch = forward/back pass through all train data
+    n_train = 320          #number of training samples, needs to be a multiple of batch size. Big memory hog.
     inv_color = 1           #use inverse color
     rescale = 1             #rescale images to increase contrast (still 0-1 normalized)
     save_models = 1         #save models
     
     ########## Parameters to Iterate Over ##########
-    filter_length = [3,3]   #See unet model. Filter length used.
-    n_filters = [64,64]     #See unet model. Arranging this so that total number of model parameters <~ 10M, otherwise OOM problems
-    lmbda = [0,0]           #See unet model. L2 Weight regularization strength (lambda).
-    init = ['he_normal', 'he_uniform']  #See unet model. Initialization of weights.
+    filter_length = [3]   #See unet model. Filter length used.
+    n_filters = [64]     #See unet model. Arranging this so that total number of model parameters <~ 10M, otherwise OOM problems
+    lmbda = [0]           #See unet model. L2 Weight regularization strength (lambda).
+    init = ['he_normal']  #See unet model. Initialization of weights.
     ########## Parameters to Iterate Over ##########
     
     #run models
